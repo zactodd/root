bf354f470ea997ef195a0714e7d60c98a277ccae
Zac Todd <zactodd0@gmail.com>
2021-09-09 22:58:48 +1200

    Update faction.py

---
+++ root/factions/faction.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import List, Set
+
+
+class Faction(ABC):
+    def __init__(self):
+        self.hand = []
+        self.score = 0
+        self.crafted_items = []
+        self.crafted_cards = []
+
+    @abstractmethod
+    def birdsong(self) -> List[Set]:
+        pass
+
+    @abstractmethod
+    def daylight(self) -> List[Set]:
+        pass
+
+    @abstractmethod
+    def evening(self) -> List[Set]:
+        pass
