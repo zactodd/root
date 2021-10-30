f301ed148a06225bd994ebdf59e3f10729c8368e
Zac Todd <zactodd0@gmail.com>
2021-10-30 17:47:39 +1300

    Update faction.py

--- root/factions/faction.py
+++ root/factions/faction.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import List, Set
+import numpy as np
+from collections import Counter
 
 
 class Faction(ABC):
@@ -9,8 +11,12 @@ class Faction(ABC):
         self.crafted_items = []
         self.crafted_cards = []
 
+        self.building_clearing = {}
+        self.warriors_clearing = Counter()
+
+
     @abstractmethod
-    def stepup(self) -> List[Set]:
+    def setup(self) -> List[Set]:
         pass
 
     @abstractmethod
@@ -24,3 +30,34 @@ class Faction(ABC):
     @abstractmethod
     def evening(self) -> List[Set]:
         pass
+
+    def move(self, clearing_from, clearing_to):
+        pass
+
+    def can_move_from(self) -> Set:
+        pass
+
+    def can_move_to(self, clearing_from) -> Set:
+        pass
+
+    def pre_battle(self):
+
+    def battle(self, clearing, faction):
+        faction.pre_battle(self)
+        if (num_warriors := self.warriors_clearing[clearing]) > 0:
+            d1, d2 = np.random.randint(3, size=2)
+            if d2 > d1:
+                d1, d2 = d2, d1
+            self_attack = min(d1, num_warriors)
+
+    def post_battle(self, faction):
+        pass
+
+    def can_battle(self, clearing):
+        pass
+
+    @abstractmethod
+    def craft(self, card) -> Set:
+        pass
+
+
